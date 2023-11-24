abbe95bbb00e32b879fbbb1458a15e40ab033535
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-11-24 03:12:05 -0600

    formato imagenes

--- HSV.py
+++ HSV.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-# Function to preprocess image
+# Función para preprocesar la imagen
 def preprocess_and_extract_features(imagepath):
     try:
         image = cv2.imread(imagepath)
@@ -13,21 +13,21 @@ def preprocess_and_extract_features(imagepath):
             print(f'Error loading image: {imagepath}')
             return None
 
-        # Convert image to HSV color space
+        # Convertir la imagen al espacio de color HSV
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        # Extract histograms for H and S channels
+        # Extraer histogramas para los canales H y S
         h_hist = cv2.calcHist([hsv_image], [0], None, [256], [0, 256])
         s_hist = cv2.calcHist([hsv_image], [1], None, [256], [0, 256])
 
-        # Feature extraction: first moment of distribution
+        # Extracción de características: primer momento de la distribución
         h_mean = np.mean(h_hist)
         s_mean = np.mean(s_hist)
 
-        return h_mean, s_mean, imagepath  # Return image path along with features
+        return h_mean, s_mean, imagepath  # Devolver la ruta de la imagen junto con las características
 
     except Exception as e:
-        print(f'Error processing image {imagepath}: {e}')
+        print(f'Error al procesar la imagen {imagepath}: {e}')
         return None
 
 
@@ -45,27 +45,24 @@ def mbsas(featuresm, threshold, min_pts):
                         added_to_cluster = True
                         break
                 except Exception as e:
-                    print(f"Error comparing features: {e}")
+                    print(f"Error al comparar caracteristicas: {e}")
                     print(f"featurem[:-1]: {featurem[:-1]}, cluster_feature[:-1]: {cluster_feature[:-1]}")
 
         if not added_to_cluster:
             new_cluster = [featurem]
             clusters.append(new_cluster)
 
-    # Filter clusters with points below the minimum threshold
+    # Filtrar clusters con puntos por debajo del umbral mínimo
     clusters = [clusterm for clusterm in clusters if len(clusterm) >= min_pts]
 
     return clusters
 
 
-# Example usage
-# Directory containing your images
-image_directory = './PinesFOPT-OPT/entrenamiento/FOPT/'
+image_directory = './PinesFOPT-OPT/entrenamiento/FOPT'
 
-# List to store features for each image
 features_list = []
 
-# Iterate over images in the directory
+# Iterar sobre las imágenes en el directorio
 for filename in os.listdir(image_directory):
     if filename.endswith(".jpg"):
         image_path = os.path.join(image_directory, filename)
@@ -74,50 +71,48 @@ for filename in os.listdir(image_directory):
         if features is not None:
             features_list.append(features)
 
-# Convert the list of features to a NumPy array
+# Convertir la lista de características a un array de numpy 20 y 10
 features = np.array(features_list)
 threshold_value = 20
 min_points = 10
-
+cluster_labels = []
 result_clusters = mbsas(features, threshold_value, min_points)
 
-# Suppose you have a ground truth for a subset of images
-ground_truth = {'./PinesFOPT-OPT/validacion/FOPT/38_cuatro.jpg': 'Madura',
-                './PinesFOPT-OPT/validacion/FOPT/2_uno.jpg': 'Verde', './PinesFOPT-OPT/validacion/FOPT/25_dos.jpg':
+ground_truth = {'./PinesFOPT-OPT/validacion/FOPT/28_cuatro.jpg': 'Madura',
+                './PinesFOPT-OPT/validacion/FOPT/2_uno.jpg': 'Verde', './PinesFOPT-OPT/validacion/FOPT/24_dos.jpg':
                     'Medio-Madura'}
 
-# Assign labels to clusters based on a majority voting scheme
-cluster_labels = []
+# Asignar etiquetas a cada característica dentro del cluster basándose en un esquema de votación mayoritaria
 for cluster in result_clusters:
-    label_counts = {label: 0 for label in set(ground_truth.values())}
     for feature in cluster:
         _, _, image_path = feature  # Desempaquetar los valores de la característica
-        true_label = ground_truth.get(image_path, 'unknown')  # Usar get para proporcionar un valor predeterminado
-        if true_label in label_counts:  # Verificar si la etiqueta está en el conjunto de etiquetas
-            label_counts[true_label] += 1
+        true_label = ground_truth.get(image_path, 'unknown')
+        label_counts = {label: 0 for label in set(ground_truth.values())}
 
-    # Assign the label with the highest count to the cluster
-    dominant_label = max(label_counts, key=label_counts.get)
-    cluster_labels.append(dominant_label)
+        for other_feature in cluster:
+            _, _, other_image_path = other_feature
+            other_true_label = ground_truth.get(other_image_path, 'unknown')
+            if other_true_label in label_counts:
+                label_counts[other_true_label] += 1
 
+        # Asignar la etiqueta con el recuento más alto a la característica individual
+        dominant_label = max(label_counts, key=label_counts.get)
+        cluster_labels.append(dominant_label)
 
 # Mostrar resultados
 for i, cluster in enumerate(result_clusters):
     print(f"Cluster {i + 1}:")
-    print(f"  Número de imágenes en el cluster: {len(cluster)}")
+    print(f"  Numero de imagenes en el cluster: {len(cluster)}")
 
     # Visualizar algunas imágenes del cluster
-    for j, feature in enumerate(cluster[:min(3, len(cluster))]):  # Visualiza hasta 3 imágenes de cada cluster
-        image_path = feature[-1]  # Last element is the image_path
-        assigned_label = cluster_labels[i]  # Use the cluster label assigned during majority voting
-
-        # Obtener la etiqueta real desde el diccionario de ground_truth
-        true_label = ground_truth.get(image_path, 'Desconocido')
+    for j, feature in enumerate(cluster[:min(5, len(cluster))]):
+        image_path = feature[-1]
+        assigned_label = cluster_labels[i * len(cluster) + j]
 
         # Mostrar la imagen junto con las etiquetas
         img = mpimg.imread(image_path)
         plt.imshow(img)
-        plt.title(f"Cluster {i + 1}, Imagen {j + 1}\nEtiqueta asignada: {assigned_label}, Etiqueta real: {true_label}")
-        plt.axis('off')  # Desactivar ejes
+        plt.title(f"Cluster {i + 1}, Imagen {j + 1}\nEstado: {assigned_label}")
+        plt.axis('off')
         plt.show()
     print("\n")
